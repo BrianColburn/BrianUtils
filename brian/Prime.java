fd926667efa4bab6451c4556f7aea5d365ae07ae
BrianColburn <BrianPColburn@gmail.com>
2017-05-28 16:13:33 -0500

    Fix primeQ

--- brian/Prime.java
+++ brian/Prime.java
@@ -5,36 +5,47 @@ import brian.functional.Memoization;
 import brian.utils.list.IntList;
 import brian.utils.list.LongList;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.LongSupplier;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.LongStream;
 
 public class Prime {
-    private static Set<Long> primes = new HashSet<>(Collections.singletonList(2L));
+    private static Set<Long> primes = new HashSet<>(Arrays.asList(2L, 3L, 5L, 7L, 11L));
+    private LongSupplier supplier = primeSupplier();
 
-    public static Predicate<Number> primeQ = (Number num) -> {
+    public Predicate<Number> primeQ = (Number num) -> {
         Long n = num.longValue();
         if (primes.contains(n)) return true;
         else if (primes.stream()
-                       .filter(p -> p <= Math.round(Math.sqrt(n)) + 1)
-                       .noneMatch(p -> n % p == 0)) {
-            primes.add(n);
-            return true;
+                       .filter(p -> p <= Math.round(Math.sqrt(n)) + 2)
+                       .anyMatch(p -> n % p == 0)) {
+            return false;
         }
-        return false;
+        long p = supplier.getAsLong();
+        primes.add(p);
+        while (p <= Math.round(Math.sqrt(n))+2) {
+            if (n % p == 0) return false;
+            p = supplier.getAsLong();
+            primes.add(p);
+        }
+        primes.add(n);
+        return true;
     };
 
-    public static Predicate<Number> memPrimeQ = Memoization.memoize(primeQ);
+    public Predicate<Number> memPrimeQ = Memoization.memoize(primeQ);
 
     /**
      * Generates primes from 2 up
      *
      * @return IntStream of primes
      */
-    public static LongStream primes() {
-        return LongStream.iterate(2, i -> i + 1).filter(primeQ::test);
+    public LongStream primes() {
+        return LongStream.generate(primeSupplier());
     }
 
     /**
@@ -45,7 +56,7 @@ public class Prime {
      *
      * @return List of primes
      */
-    public static LongList primeList(Number n) {
+    public LongList primeList(Number n) {
         return new LongList(primes().limit(n.longValue()));
     }
 
@@ -55,12 +66,17 @@ public class Prime {
      *        Index
      * @return The <i>n</i>th prime assuming a 1 based index
      */
-    public static Long primeN(Number n) {
+    public Long primeN(Number n) {
         return primes()
                 .limit(n.longValue())
                 .skip(n.longValue() - 1)
                 .findFirst()
                 .getAsLong();
+    }
+
+    public LongSupplier primeSupplier() {
+        InfIterator<Long> iterator = primeIterator();
+        return iterator::next;
     }
 
     public InfIterator<Long> primeIterator() {
